42d6a50c1042b9b86dffbc8acd59d5817faafd5b
Zachary Vance <za3k@za3k.com>
2023-11-30 20:11:21 -0500

    Add music

---
+++ README.md
@@ -0,0 +1,3 @@
+nsh and I wrote "music of the spheres". they came up with the idea and did the music, i added a little bit of programming.
+
+demo available here [here](https://za3k.github.io/ha3k-30-musicofspheres)
--- simulation.js
+++ simulation.js
@@ -1,4 +1,6 @@
-const PERIOD_SCALE = 0.5;
+const PERIOD_SCALE = 0.2;
+const SMALLEST_PLANET = 50;
+const RADIUS_SCALE = 1.2;
 const PLANET_COLORS = [
     "#9E9E9E", // Mercury
     "#FFFACD", // Venus
@@ -18,41 +20,29 @@ const PLANET_RADIUS = [
 let canvas = document.getElementById('simulationCanvas');
 let ctx = canvas.getContext('2d');
 let planets = [];
+let trail = [];
 
-function setupPlanets() {
-    let numPlanets = document.getElementById('numPlanets').value;
-    planets = [];
-    for (let i = 0; i < numPlanets; i++) {
-	planets.push({ radius: 100 + i * 50, period: 100 + i * 50, angle: 0 });
-    }
-    renderPlanetSettings();
+function seededRandom(seed) {
+    let s = Math.sin(seed) * 10000;
+    return s - Math.floor(s);
 }
-
-function renderPlanetSettings() {
-    let settingsDiv = document.getElementById('planetSettings');
-    settingsDiv.innerHTML = '';
-    planets.forEach((planet, index) => {
-    settingsDiv.innerHTML += `<div>
-    <label>Planet ${index + 1} - Radius: </label>
-    <input type="number" onchange="updatePlanetRadius(${index}, this.value)" value="${planet.radius}">
-    </div>`;
-    });
-}
-
-function updatePlanetRadius(index, value) {
-    planets[index].radius = planets[index].period = parseInt(value);
-}
-
 
 function draw() {
-    //canvas.height = canvas.width = (maxRadius*2)
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let centerX = canvas.width / 2;
     let centerY = canvas.height / 2;
 
+	// Starry background
+	for (let i=0; i<1000; i++) {
+		const x = seededRandom(i) * canvas.width;
+		const y = seededRandom(i+5000) * canvas.height;
+		ctx.fillStyle = "white";
+		ctx.fillRect(x, y, 1, 1);
+	}
+
     // Draw the star
     ctx.beginPath();
-    ctx.arc(centerX, centerY, 20, 0, 2 * Math.PI); // Slightly larger circle for the star
+    ctx.arc(centerX, centerY, 40, 0, 2 * Math.PI); // Slightly larger circle for the star
     ctx.fillStyle = 'yellow';
     ctx.fill();
 
@@ -60,40 +50,130 @@ function draw() {
     planets.forEach((planet, i) => {
 	    let x = centerX + planet.radius * Math.cos(planet.angle);
 	    let y = centerY + planet.radius * Math.sin(planet.angle);
+        let planetRadius = PLANET_RADIUS[i % PLANET_RADIUS.length];
+        const planetColor = PLANET_COLORS[i % PLANET_COLORS.length];
+        if (planet.highlight) {
+            planetRadius *= 1.5;
+            ctx.lineWidth = 3;
+            ctx.beginPath();
+            ctx.arc(x, y, planetRadius+5, 0, 2 * Math.PI); // Draw planet
+            ctx.strokeStyle = "green";
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(x, y, planetRadius+10, 0, 2 * Math.PI); // Draw planet
+            ctx.strokeStyle = planetColor;
+            ctx.stroke();
+            ctx.lineWidth = 1;
+        }
 	    ctx.beginPath();
-	    ctx.arc(x, y, PLANET_RADIUS[i % PLANET_RADIUS.length], 0, 2 * Math.PI); // Draw planet
-	    ctx.fillStyle = PLANET_COLORS[i % PLANET_COLORS.length];
+	    ctx.arc(x, y, planetRadius, 0, 2 * Math.PI); // Draw planet
+	    ctx.fillStyle = planetColor;
 	    ctx.fill();
 
 	    planet.angle += PERIOD_SCALE * 2 * Math.PI / planet.period; // Update angle for next frame
     });
+	trail.forEach(planet => {
+	    let x = centerX + planet.radius * Math.cos(planet.angle);
+	    let y = centerY + planet.radius * Math.sin(planet.angle);
+		ctx.lineWidth = 3;
+	    ctx.beginPath();
+	    ctx.arc(x, y, planet.size, 0, 2 * Math.PI); // Draw planet
+	    ctx.strokeStyle = planet.color;
+	    ctx.stroke();
+		ctx.globalAlpha = 0.5;
+		ctx.fillStyle = planet.color
+	    ctx.fill();
+		ctx.globalAlpha = 1;
+		ctx.lineWidth = 1;
+	});
 
     requestAnimationFrame(draw);
 }
 
 window.onload = (ev) => {
-    setupPlanets();
     requestAnimationFrame(draw);
 };
 
 function usePreset() {
     let preset = document.getElementById('presets').value;
     if (!preset) return;
-    const fractions = preset.split(" ");
-    setPlanetsFromScale(fractions.map((fractionString) => {
+    const ratios = preset.split(" ").map((fractionString) => {
         const [num, denom] = fractionString.split("/");
         const decimal = parseInt(num) / parseInt(denom);
         return decimal;
-    }));
+    });
+    setPlanetsFromScale(ratios)
+    playSong();
 }
 
 function setPlanetsFromScale(ratios) {
-    let cum = 50;
+    let period = SMALLEST_PLANET;
+	let radius = SMALLEST_PLANET;
     planets = [];
-    planets.push({ radius: cum, period: cum, angle: 0 });
+	trail = [];
     for (let ratio of ratios) {
-        cum *= ratio;
-        planets.push({ radius: cum, period: cum, angle: 0 });
+        period *= ratio;
+        radius *= ratio * RADIUS_SCALE;
+		planets.push({ radius, period, angle: 0 });
     }
-    renderPlanetSettings();
+}
+
+function sleep(timeMs) {
+  return new Promise((resolve) => setTimeout(resolve, timeMs));
+}
+
+function playTone(frequency, ms) {
+    // Create an audio context
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    // Create an oscillator (a sound wave)
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+    // Set the frequency (pitch) of the tone
+    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+    gainNode.gain.setValueAtTime(0.005, audioContext.currentTime); // Adjust the volume here (0.0 to 1.0)
+
+    // Connect the oscillator to the gain node, and the gain node to the audio context's destination
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    // Start the oscillator to play the tone
+    oscillator.start();
+    // Stop the oscillator after a certain duration (in this case, after 1 second)
+    oscillator.stop(audioContext.currentTime + ms/1000);
+    return sleep(ms);
+}
+
+function freqFromNote(note, planet) {
+	return scale = planet.period / SMALLEST_PLANET * 261;
+}
+
+async function playNote(note, length) {
+    const planet = planets[note-1] || {};
+	trail.push({
+		angle: planet.angle,
+		radius: planet.radius,
+		size: PLANET_RADIUS[note-1],
+		color: PLANET_COLORS[note-1],
+	});
+    planet.highlight = 1;
+    const freq = freqFromNote(note, planet);
+    await playTone(freq, length*420);
+	planet.highlight = 0;
+    await sleep(length*80);
+}
+
+async function playSong() {
+	let melody = [
+		[3, 1], [2, 1], [1, 1], [2, 1],  // Mary had a 
+		[3, 1], [3, 1], [3, 2],          // little lamb, 
+		[2, 1], [2, 1], [2, 2],          // little lamb,
+		[3, 1], [5, 1], [5, 2],          // little lamb. Its 
+		[3, 1], [2, 1], [1, 1], [2, 1],  // Mary had a 
+		[3, 1], [3, 1], [3, 1], [3, 1],  // little lamb. It's fleece
+		[2, 1], [2, 1], [3, 1], [2, 1],  // fleece was white as 
+		[1, 4],                          // snow.
+	];
+    for (let [note, length] of melody) {
+        await playNote(note, length);
+    }
 }
